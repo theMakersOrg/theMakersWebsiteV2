fb747faa0523f88e19bbb6ed71bfa0601787d713
Kurt Schoenhoff <Kurt@kurtsch.com.au>
2018-12-11 13:15:12 +1000

    adjusted her.svg as well as styling for hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,13 @@ import hero from '../img/hero.svg'
 
 const Hero = class extends React.Component {
   render() {
-    return <img src={hero} alt="theMakers.org" style={{ width: '1024px' }} />
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div>
+          <img src={hero} alt="theMakers.org" style={{ width: '1024px' }} />
+        </div>
+      </div>
+    )
   }
 }
 export default Hero
