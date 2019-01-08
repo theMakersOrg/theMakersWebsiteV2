c849bc440f6f3784ac59718b558e2ce853a9808a
Kurt Schoenhoff <Kurt@kurtsch.com.au>
2019-01-08 17:25:10 +1000

    adjusting Css to suite site colours for the box

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,8 +39,8 @@ const IndexPageTemplate = ({
                 <h2
                   className="has-text-weight-bold is-size-1"
                   style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
+                    boxShadow: '0.5rem 0 0 $primary-invert, -0.5rem 0 0 $primary-invert',
+                    backgroundColor: '$primary-invert',
                     color: 'white',
                     padding: '1rem'
                   }}
