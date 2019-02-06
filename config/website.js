0ad187ac8fe6675394229248093b7a64d8656ac1
Kurt Schoenhoff <Kurt@kurtsch.com.au>
2019-02-06 14:56:56 +1000

    adjusting the manifrest

--- config/website.js
+++ config/website.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'theMakers.org - Smart Community, shared resources', // Navigation and Site Title
-  titleAlt: 'The Makers theMakers.org', // Title for JSONLD
+  titleAlt: 'theMakers.org', // Title for JSONLD
   description:
     'TheMakers.org is a Not-For-Profit Maker Space Located in Cairns, QLD, that strives to give the community access to cutting edge technology, resources and know-how.',
   headline: 'Makerspace, hackerspace, your space', // Headline for schema.org JSONLD
